6ed145844355739a7337cae267731d4102ab0e6d
Neeraj Kumar <neerajpkumar@gmail.com>
2012-04-07 19:52:56 +0530

    Adding login logic

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/codeig';
+$config['base_url']	= 'http://localhost/fcommerce';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '111111555555665456541235554';
 
 /*
 |--------------------------------------------------------------------------
